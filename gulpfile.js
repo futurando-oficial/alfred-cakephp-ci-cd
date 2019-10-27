87b6df3b78b35fbc7a1bdcfe015fc62039a0bcac
Gustavo Camillo Assis <guto00741@yahoo.com.br>
2019-10-27 20:37:22 -0300

    configured Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var sourcemaps = require('gulp-sourcemaps');
 var open = require('gulp-open');
+var cleanCSS = require('gulp-clean-css');
 
 var Paths = {
   HERE: './',
@@ -16,6 +17,7 @@ gulp.task('compile-scss', function() {
   return gulp.src(Paths.SCSS_TOOLKIT_SOURCES)
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(autoprefixer())
     .pipe(sourcemaps.write(Paths.HERE))
     .pipe(gulp.dest(Paths.CSS));
